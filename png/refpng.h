da52df964ed065e7816a6419d2fb60c6bab9c889
Ajay Joshi <ajsj7598@gmail.com>
2022-11-07 16:36:43 -0600

    Add support for ppm images from Image Compression benchmark

--- png/refpng.h
+++ png/refpng.h
@@ -11,8 +11,8 @@ extern png_byte color_type;
 extern png_byte bit_depth;
 extern png_bytep *row_pointers;
 
-void read_png_file(char *filename);
-void write_png_file(char *filename);
+void read_png_file(const char *filename);
+void write_png_file(const char *filename);
 
 void copy_row_pointers_to_vector(std::vector<uint8_t>& image, png_bytep *row_pointers, int width, int height);
 png_bytep* copy_vector_to_row_pointers(std::vector<uint8_t>& image, int width, int height);
