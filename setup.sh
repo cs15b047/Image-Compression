ea702231e32a34de8729ecc04954a2dc1ecb4e72
ajsj7598 <ajsj7598@node0.cvproject.advosuwmadison-pg0.wisc.cloudlab.us>
2022-12-01 03:29:44 -0600

    Modified setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.9
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
-sudo apt install python3-pip
-pip3 install diffusers accelerate libimagequant mozjpeg-lossless-optimization scikit-image tqdm tensorflow-compression huggingface-hub Pillow transformers ftfy -t /mnt/Work/
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu -t /mnt/Work/packagesq
+sudo apt install python3-pip python3.9-distutils
+pip3 install diffusers accelerate libimagequant mozjpeg-lossless-optimization scikit-image tqdm tensorflow-compression huggingface-hub Pillow transformers ftfy -t /mnt/
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu -t /mnt/
